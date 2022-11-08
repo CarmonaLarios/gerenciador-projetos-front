19f535d7dfc28a8fd8550f8579dd3882059ce2e1
Renan Larios <renancarmona94@live.com>
2022-11-08 18:22:53 -0300

    Adicionado página de projetos e tela de loading

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,16 @@
-import { useLocation } from "react-router-dom";
-import Message from "../layout/Message";
+import { useLocation } from "react-router-dom"
+import Message from "../layout/Message"
+import Container from "../layout/Container"  
+import Loading from "../layout/Loading"
+import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
+import styles from "./Projects.module.css"
+import { useState, useEffect } from "react"
 
 function Projects(){
+    const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
+    
     const location = useLocation()
     let message = ''
 
@@ -9,10 +18,41 @@ function Projects(){
         message = location.state.message
     }
 
+    useEffect(()=>{
+      fetch('http://localhost:5000/projects', {
+        method: 'GET',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+        setProjects(data)
+        setRemoveLoading(true)
+      })
+      .catch(err =>console.log(err))
+    },[])
+
     return (
-      <div>
-        <h1>Projects</h1>
-        <Message type="sucess" message={message}/>
+      <div className={styles.project_container}>
+        <div className={styles.title_container}>
+          <h1>Projects</h1>
+          <LinkButton to="/newproject" text="Criar projeto"/>
+        </div>
+        {message && <Message type="sucess" message={message}/>}
+        <Container customClass="start">
+          {projects.length > 0 &&
+            projects.map(project => ( 
+              <ProjectCard 
+                id={project.id}
+                name={project.name}
+                budget={project.budget}
+                category={project.category.name}
+                key={project.id}
+              />
+            ))}
+            {!removeLoading && <Loading/>}
+        </Container>
       </div>
     );
 }
